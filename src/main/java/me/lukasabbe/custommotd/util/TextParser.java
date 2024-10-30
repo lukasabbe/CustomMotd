bc77d6cebcbd1322e6fa39d2bdfe6f42741efe9b
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-10-30 23:58:10 +0100

    Support for Simplified Text Format

--- src/main/java/me/lukasabbe/custommotd/util/TextParser.java
+++ src/main/java/me/lukasabbe/custommotd/util/TextParser.java
@@ -2,14 +2,15 @@ package me.lukasabbe.custommotd.util;
 
 import eu.pb4.placeholders.api.PlaceholderContext;
 import eu.pb4.placeholders.api.Placeholders;
+import eu.pb4.placeholders.api.TextParserUtils;
 import me.lukasabbe.custommotd.Custommotd;
 import net.minecraft.text.Text;
 
 public class TextParser {
-    public static String formatText(String text){
+    public static Text formatText(String text){
         text = text.replace("\\u00A7", "§");
         text = text.replace("\\n","\n");
         text = Placeholders.parseText(Text.of(text), PlaceholderContext.of(Custommotd.server)).getString();
-        return text;
+        return TextParserUtils.formatText(text);
     }
 }
